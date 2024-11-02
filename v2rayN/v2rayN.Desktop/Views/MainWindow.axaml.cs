4e4106ffc7ddcc631c38c92c1edaff2f1b05adf2
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2024-11-02 18:35:20 +0800

    Update MainWindow.axaml.cs

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -307,11 +307,6 @@ namespace v2rayN.Desktop.Views
             ShowHideWindow(false);
         }
 
-        private void menuPromotion_Click(object? sender, RoutedEventArgs e)
-        {
-            Utils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
-        }
-
         private void menuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
         {
             Utils.ProcessStart(Utils.GetBinPath("EnableLoopback.exe"));
