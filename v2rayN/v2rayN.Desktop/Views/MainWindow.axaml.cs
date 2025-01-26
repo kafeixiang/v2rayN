b96fc33baef14172d6cb9f39c8dac6521a494e79
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2025-01-26 17:57:57 +0800

    Update MainWindow.axaml.cs

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -32,7 +32,6 @@ namespace v2rayN.Desktop.Views
 
             this.KeyDown += MainWindow_KeyDown;
             menuSettingsSetUWP.Click += menuSettingsSetUWP_Click;
-            menuPromotion.Click += menuPromotion_Click;
             menuCheckUpdate.Click += MenuCheckUpdate_Click;
             menuBackupAndRestore.Click += MenuBackupAndRestore_Click;
             menuClose.Click += MenuClose_Click;
@@ -330,11 +329,6 @@ namespace v2rayN.Desktop.Views
                     ViewModel?.Reload();
                 }
             }
-        }
-
-        private void menuPromotion_Click(object? sender, RoutedEventArgs e)
-        {
-            ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
         }
 
         private void menuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
