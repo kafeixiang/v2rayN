e5792c9991ab8e1f38de97e96283c4e50870744e
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2025-04-27 16:21:59 +0800

    qg

--- v2rayN/v2rayN/Views/MainWindow.xaml.cs
+++ v2rayN/v2rayN/Views/MainWindow.xaml.cs
@@ -22,7 +22,6 @@ public partial class MainWindow
         this.Closing += MainWindow_Closing;
         this.PreviewKeyDown += MainWindow_PreviewKeyDown;
         menuSettingsSetUWP.Click += menuSettingsSetUWP_Click;
-        menuPromotion.Click += menuPromotion_Click;
         menuClose.Click += menuClose_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuBackupAndRestore.Click += MenuBackupAndRestore_Click;
@@ -301,11 +300,6 @@ public partial class MainWindow
     {
         StorageUI();
         ShowHideWindow(false);
-    }
-
-    private void menuPromotion_Click(object sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void menuSettingsSetUWP_Click(object sender, RoutedEventArgs e)
